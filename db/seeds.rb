3d2ea53c7366c7479a6c11b147cd0c9ddad95ce7
Alexis Hoffman <azguard@iinet.net.au>
2016-07-15 15:37:55 +0800

    Add bootstrapped specs, seeds, and project info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,57 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
 
 User.destroy_all
-Profile.destroy_all
+TodoList.destroy_all
 
-User.create! [
-	{ username: "Fiorina", password_digest: "pass" },
-	{ username: "Trump", password_digest: "pass" },
-	{ username: "Carson", password_digest: "pass" },
-	{ username: "Clinton", password_digest: "pass" },
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
+
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
+end
+
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
 ]
 
-User.find_by(username: "Fiorina").create_profile(gender: "female", birth_year: 1954, first_name: "Carly", last_name: "Fiorina")
-User.find_by(username: "Trump").create_profile(gender: "male", birth_year: 1946, first_name: "Donald", last_name: "Trump")
-User.find_by(username: "Carson").create_profile(gender: "male", birth_year: 1951, first_name: "Ben", last_name: "Carson")
-User.find_by(username: "Clinton").create_profile(gender: "female", birth_year: 1947, first_name: "Hillary", last_name: "Clinton")
-
-User.find_by(username: "Fiorina").todo_lists.create!(list_name: "list1", list_due_date: Date.today + 1.year)
-User.find_by(username: "Trump").todo_lists.create!(list_name: "list2", list_due_date: Date.today + 1.year)
-User.find_by(username: "Carson").todo_lists.create!(list_name: "list3", list_due_date: Date.today + 1.year)
-User.find_by(username: "Clinton").todo_lists.create!(list_name: "list4", list_due_date: Date.today + 1.year)
-
-
-TodoList.first.todo_items.create! [
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-]
-TodoList.second.todo_items.create! [
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-]
-TodoList.third.todo_items.create! [
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-]
-TodoList.fourth.todo_items.create! [
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-  { title: "title", description: "descn", due_date: Date.today + 1.year },
-]
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
+end
